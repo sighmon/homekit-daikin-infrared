d3e2f14ad30caa0cdf30893fcaa2d2f46dd0c762
sighmon <simon.loffler@gmail.com>
2022-06-26 12:56:00 +0930

    Add HAP Heater accessory

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/sighmon/homekit-daikin-infrared
+
+go 1.18
+
+require github.com/brutella/hap v0.0.15
+
+require (
+	github.com/brutella/dnssd v1.2.1 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/miekg/dns v1.1.1 // indirect
+	github.com/tadglines/go-pkgs v0.0.0-20210623144937-b983b20f54f9 // indirect
+	github.com/xiam/to v0.0.0-20200126224905-d60d31e03561 // indirect
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ homekit-daikin-infrared.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/brutella/hap"
+	"github.com/brutella/hap/accessory"
+
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// Create the Daikin heater accessory.
+	a := accessory.NewHeater(accessory.Info {
+		Name: "Daikin air conditioner",
+		SerialNumber: "FTXS50KAVMA",
+		Manufacturer: "Daikin",
+		Model: "FTXS50KAVMA",
+		Firmware: "1.0.0",
+	})
+
+	a.Heater.Active.OnValueRemoteUpdate(func(on int) {
+		if on == 1 {
+			log.Println("TODO: send on command")
+		} else {
+			log.Println("TODO: send off command")
+		}
+	})
+
+	// Store the data in the "./db" directory.
+	fs := hap.NewFsStore("./db")
+
+	// Create the hap server.
+	server, err := hap.NewServer(fs, a.A)
+	if err != nil {
+		// stop if an error happens
+		log.Panic(err)
+	}
+
+	// Setup a listener for interrupts and SIGTERM signals
+	// to stop the server.
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		<- c
+		// Stop delivering signals.
+		signal.Stop(c)
+
+		// Cancel the context to stop the server.
+		cancel()
+	}()
+
+	// Run the server.
+	server.ListenAndServe(ctx)
+}
