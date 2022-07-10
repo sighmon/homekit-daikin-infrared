f21e2b7a44b081c876cea6ce7ba325d11eec0993
sighmon <simon.loffler@gmail.com>
2022-07-10 11:07:16 +0930

    # Add swing mode

--- homekit-daikin-infrared.go
+++ homekit-daikin-infrared.go
@@ -20,12 +20,14 @@ import (
 var currentFanSpeed float64
 var currentHeaterCoolerState int
 var currentHeatingThresholdTemperature float64
+var currentSwingMode int
 var developmentMode bool
 var dyson bool
 var fs hap.Store
 var ir *lirc.Router
 var lircName string
 var fanSpeed *characteristic.RotationSpeed
+var swingMode *characteristic.SwingMode
 
 func init() {
 	flag.BoolVar(&developmentMode, "dev", false, "development mode, so ignore LIRC setup")
@@ -64,6 +66,13 @@ func init() {
 	}
 	storedFanSpeedInt, _ := strconv.Atoi(string(storedFanSpeed))
 	currentFanSpeed = float64(storedFanSpeedInt)
+	storedSwingMode, err := fs.Get("currentSwingMode")
+	if err != nil {
+		fs.Set("currentSwingMode", []byte("0"))
+		storedSwingMode = []byte("0")
+	}
+	storedSwingModeInt, _ := strconv.Atoi(string(storedSwingMode))
+	currentSwingMode = storedSwingModeInt
 }
 
 func sendLircCommand(command string) {
@@ -122,6 +131,7 @@ func main() {
 			a.Heater.HeatingThresholdTemperature.SetValue(currentHeatingThresholdTemperature)
 			a.Heater.TargetHeaterCoolerState.SetValue(currentHeaterCoolerState)
 			fanSpeed.SetValue(currentFanSpeed * 10)
+			swingMode.SetValue(currentSwingMode)
 			sendLircCommand(powerOnCommand)
 		} else {
 			log.Println("Sending power off command")
@@ -194,6 +204,22 @@ func main() {
 		fs.Set("currentFanSpeed", []byte(fmt.Sprintf("%d", int(currentFanSpeed))))
 	})
 	a.Heater.AddC(fanSpeed.C)
+
+	// Add swing mode
+	swingMode = characteristic.NewSwingMode()
+	swingMode.SetValue(currentSwingMode)
+	swingMode.OnValueRemoteUpdate(func(value int) {
+		if dyson {
+			if currentSwingMode != value {
+				sendLircCommand(fmt.Sprintf("%s OSCILLATION", lircName))
+			}
+		} else {
+			// TODO: Daikin swing mode
+		}
+		log.Println(fmt.Sprintf("Sending %s swing mode command: %d", lircName, value))
+		currentSwingMode = value
+		fs.Set("currentSwingMode", []byte(fmt.Sprintf("%d", currentSwingMode)))
+	})
 
 	// Create the hap server.
 	server, err := hap.NewServer(fs, a.A)
